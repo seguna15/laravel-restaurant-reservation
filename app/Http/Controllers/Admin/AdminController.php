3210d6438e810c9e2b7b24d04da8bc1b45c08389
seguna15 <seguna15@gmail.com>
2022-04-14 12:36:38 +0100

    first commit

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    //index function
+    public function index()
+    {
+        return view('admin.index');
+    }
+}
